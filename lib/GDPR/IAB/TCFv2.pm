558de061e37308a89a5a55a79ef722ca37ebe47b
Tiago Peczenyj <tpeczenyj@weborama.com>
2026-05-05 15:04:47 +0200

    feat: implement unified subcommand-based CLI and bump version to 0.310

--- lib/GDPR/IAB/TCFv2.pm
+++ lib/GDPR/IAB/TCFv2.pm
@@ -22,7 +22,7 @@ use GDPR::IAB::TCFv2::BitUtils qw<is_set
 use GDPR::IAB::TCFv2::Publisher;
 use GDPR::IAB::TCFv2::RangeSection;
 
-our $VERSION = "0.300";
+our $VERSION = "0.310";
 
 use constant {
     CONSENT_STRING_TCF_V2 => {
