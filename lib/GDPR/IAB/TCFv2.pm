d722bae8defbe222471a222c28388b44f79ae19a
Tiago Peczenyj <tpeczenyj@weborama.com>
2026-05-15 18:18:03 +0200

    docs: update disclaimer about maintenance mode

--- lib/GDPR/IAB/TCFv2.pm
+++ lib/GDPR/IAB/TCFv2.pm
@@ -43,8 +43,8 @@ GDPR::IAB::TCFv2 - TCF v2.3 distribution: parser, validator, CMP-validator, and 
 
 =head1 PROJECT STATUS
 
-C<GDPR::IAB::TCFv2> entered B<maintenance mode> on 2026-05-14 with the
-v0.500 release. The core parser, validator, and CMP-validator surfaces
+C<GDPR::IAB::TCFv2> entered B<maintenance mode> on 2026-05-15 with the
+v0.512 release. The core parser, validator, and CMP-validator surfaces
 are considered feature-complete for the IAB TCF v2.3 specification.
 
 In maintenance mode the maintainer commits to bug fixes, security
